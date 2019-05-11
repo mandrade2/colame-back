6f3994fef5af958d83874a4baaae5e12aba92d36
tiborchers <tiborchers@uc.cl>
2019-05-11 12:58:32 -0400

    refactoring imports

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+import Companies from '../controllers/company';
+import Lines from '../controllers/line';
+import Attendants from '../controllers/attendant';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+export default (app) => {
+  app.get('/', (req, res) => {
+    res.status(200).send({
+      success: true,
+      message: 'Welcome to the Colame API!',
+    });
+  });
+  /* COMPANY */
+  app.post('/company', Companies.create);
+  app.get('/company', Companies.list);
+  /* LINE */
+  app.post('/line', Lines.create);
+  app.get('/line', Lines.list);
+  /* attendants */
+  app.post('/attendant', Attendants.create);
+  app.get('/attendant', Attendants.list);
+};
