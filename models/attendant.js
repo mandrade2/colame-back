7f536c61f3e0667f96306c016d0897cb6092f674
Matías Andradde <mandrade2@uc.cl>
2019-07-03 03:39:30 -0400

    new model parameters

--- models/attendant.js
+++ models/attendant.js
@@ -4,16 +4,32 @@ const { Schema } = mongoose;
 
 const AttendantSchema = new Schema({
   name: String,
-  lines: [{ type: Schema.Types.ObjectId, ref: 'Line' }],
-  avgTimeAttention: { type: Number, default: 0 },
-  companyId: { type: Schema.Types.ObjectId, ref: 'Company' },
+  username: String,
+  password: String,
+  lines: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Line',
+    },
+  ],
+  avgTimeAttention: {
+    type: Number,
+    default: 0,
+  },
+  companyId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Company',
+  },
 });
 
-AttendantSchema.index({
-  name: 1,
-  companyId: 1,
-}, {
-  unique: true,
-});
+AttendantSchema.index(
+  {
+    name: 1,
+    companyId: 1,
+  },
+  {
+    unique: true,
+  },
+);
 
 module.exports = mongoose.model('Attendant', AttendantSchema);
--- models/company.js
+++ models/company.js
@@ -1,17 +1,27 @@
 import mongoose from 'mongoose';
 
-const { Schema } = mongoose;
+const {
+  Schema
+} = mongoose;
 
 const CompanySchema = new Schema({
   name: {
+    username: String,
+    password: String,
     type: String,
     required: [true, 'Se requiere el nombre de la empresa'],
     unique: true,
     dropDups: true,
   },
   description: String,
-  attendants: [{ type: Schema.Types.ObjectId, ref: 'Attendant' }],
-  lines: [{ type: Schema.Types.ObjectId, ref: 'Line' }],
+  attendants: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Attendant'
+  }],
+  lines: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Line'
+  }],
 });
 
 module.exports = mongoose.model('Company', CompanySchema);
